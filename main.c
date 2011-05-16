6bc3f0140c7a79ec04748ba51cd12a7d5fa19f53
simrangill <simran@engageinc.com>
2011-05-16 15:38:48 -0700

    Testing!!

---
+++ main.c
@@ -0,0 +1,21 @@
+//////////////////////////
+//	Project Stationery  //
+//////////////////////////
+
+#include <stdio.h>
+
+int main()
+{
+	int i=0;
+			
+	unsigned long proc_id;
+	
+	asm ("mfpir %0" : "=r" (proc_id));
+
+	printf("Core%d: Welcome to CodeWarrior!\r\n", proc_id>>5);
+
+	asm("sc"); // generate a system call exception to demonstrate the ISR
+		
+	while (1) { i++; } // loop forever
+}
+
